24b9da8df846028db7de3a3ec46142384bb6592a
adigavageesh95 <32138429+adigavageesh95@users.noreply.github.com>
2023-04-14 16:34:20 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -11,6 +11,12 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+router.post('/app', (req, res) => {
+  console.log(req);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
